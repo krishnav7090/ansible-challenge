2310a1077650903cf675fbef94b4eb55d5ef3987
krishnav7090 <krishna.v7090@gmail.com>
2026-02-03 20:11:29 +0530

    Modify inventory.tf for public IPs and ansible_user
    Updated inventory file to use public IPs and added ansible_user.

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -3,10 +3,10 @@ resource "local_file" "ansible_inventory" {
 
   content = <<EOF
 [frontend]
-c8.local ansible_host=${aws_instance.amazon_vm.public_ip}
+c8.local ansible_host=${aws_instance.amazon_vm.public_ip} ansible_user=ec2-user
 
 [backend]
-u21.local ansible_host=${aws_instance.ubuntu_vm.private_ip}
+u21.local ansible_host=${aws_instance.ubuntu_vm.public_ip} ansible_user=ubuntu
 
 [backend:vars]
 backend_ip=${aws_instance.ubuntu_vm.private_ip}
